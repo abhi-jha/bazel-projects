1bcd6fc9d7b982994d15af5c9f77c0f28986e6b3
Abhishek Jha <abhi_jha1995@rediffmail.com>
2023-01-19 04:05:26 +0100

    test java package building

--- Info.md
+++ Info.md
@@ -1,2 +1,9 @@
 bazel help
+
 bazel build //:hello-world
+
+bazel query  --notool_deps --noimplicit_deps "deps(//:hello-world)" --output graph
+
+bazel clean --expunge
+
+
--- first/my-app/src/main/java/com/mycompany/anotherapp/AnotherApp.java
+++ first/my-app/src/main/java/com/mycompany/anotherapp/AnotherApp.java
@@ -1,7 +1,10 @@
-package src.main.java.com.mycompany.anotherapp;
+package com.mycompany.anotherapp;
 
 public class AnotherApp {
     public static void main(String[] args) {
         System.out.println("Hello Another App");
+        for(int i = 0 ; i < 100; i++){
+            System.out.println(i + " "+ i);
+        }
     }
 }
